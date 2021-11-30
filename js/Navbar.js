279f76c4a15dfff57983f06881eec8d1fd36213b
Leroy Bryan <91132339+Az-Back@users.noreply.github.com>
2021-11-30 11:39:26 +0100

    Add files via upload

---
+++ js/Navbar.js
@@ -0,0 +1,13 @@
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+    
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
