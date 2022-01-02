117f0d5952b9f856c6880c0b8b86e68c45cf442c
Leroy Bryan <91132339+Az-Back@users.noreply.github.com>
2022-01-02 17:03:21 +0100

    Delete PortfolioEnd directory

--- PortfolioEnd/js/button.js
+++
@@ -1,8 +0,0 @@
-function myFunction1() {
-  var x = document.querySelector(".grille1");
-  if (x.style.display === "grid") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "grid";
-  }
-}
